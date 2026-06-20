6bcae9ef71fc65804e0c063e85fe423460c8ec84
Daniel Cobos <dcobosm@gmail.com>
2026-06-20 09:03:43 -0400

    Limit ServiciosHome to 4 cards, full list only on /servicios

--- src/components/ServiciosHome.tsx
+++ src/components/ServiciosHome.tsx
@@ -58,7 +58,7 @@ export default function ServiciosHome() {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          {servicios.map((service, i) => (
+          {servicios.slice(0, 4).map((service, i) => (
             <ServiceCard key={service.title} service={service} index={i} cotizarLabel={t("servicios-home.cotizar")} />
           ))}
         </div>
